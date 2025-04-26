d5d5c010e43958ef19dc1befff19c5b4a9b5234a
teddyliu1025 <teddyliu02.1025@gmail.com>
2025-04-27 00:07:03 +0800

    Enable the program to read data.

--- testcase/test_plummer/plot_gas.py
+++ testcase/test_plummer/plot_gas.py
@@ -1,9 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-# load data named "Initial.dat"
-file = "Initial.dat"
-content = np.loadtxt(file)
+# Load the file
+with open('Initial.dat', 'r') as f:
+    lines = f.readlines()
 
-print(content.size())
+# Parse the number of particles
+num_particles = int(lines[0].strip())
+
+# Masses: lines 2 to 4
+masses = np.array([float(lines[i].strip()) for i in range(1, 1 + num_particles)])
+
+# Positions: lines 5 to 7
+positions = np.array([
+    list(map(float, lines[i].strip().split()))
+    for i in range(1 + num_particles, 1 + 2 * num_particles)
+])
+
+# Velocities: lines 8 to 10
+velocities = np.array([
+    list(map(float, lines[i].strip().split()))
+    for i in range(1 + 2 * num_particles, 1 + 3 * num_particles)
+])
+
+print("Masses array:")
+print(masses)
+
+print("\nPositions array:")
+print(positions)
+
+print("\nVelocities array:")
+print(velocities)
