1a635de666bf0c14745e6c533c263eb3745642d9
linyc253 <linyc253@gmail.com>
2025-05-01 10:43:39 +0800

    Create test_3body to demonstrate the advantage of adaptive time step

---
+++ testcase/test_3body/Energy.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameter
+N_f = 301
+G = 1
+
+# array
+filename = "00001.dat"
+with open(filename) as f:
+    lines = f.readlines()
+N = int(lines[0].strip())
+x = np.empty((N_f,N,3))
+v = np.empty((N_f,N,3))
+
+# Load the file
+for i in range(1, N_f):
+    filename = "%05d" % i
+    # Load the file
+    with open(filename+".dat", 'r') as f:
+        lines = f.readlines()
+
+    # Masses:
+    m = np.array([float(lines[i].strip()) for i in range(1, 1 + N)])
+
+    # Positions:
+    pos = np.array([
+        list(map(float, lines[i].strip().split()))
+        for i in range(1 + N, 1 + 2 * N)
+    ])
+
+    # Velocities:
+    vel = np.array([
+        list(map(float, lines[i].strip().split()))
+        for i in range(1 + 2 * N, 1 + 3 * N)
+    ])
+    for j in range(N):
+        x[i,j,:] = pos[j,:]
+        v[i,j,:] = vel[j,:]
+
+E_k = np.zeros(N_f-1)
+U_g = np.zeros(N_f-1)
+E_t = np.zeros(N_f-1)
+for i in range(1,N_f):
+    for j in range(N-1):
+        E_k[i-1] += 0.5 * m[j] * (v[i,j,0]**2 + v[i,j,1]**2 + v[i,j,2]**2)
+        for k in range(j+1,N):
+            U_g[i-1] -= G*m[j]*m[k]/((x[i,j,0]-x[i,k,0])**2 + (x[i,j,1]-x[i,k,1])**2 +\
+                                 (x[i,j,2]-x[i,k,2])**2)**0.5
+    E_k[i-1] += 0.5 * m[N-1] * (v[i,N-1,0]**2 + v[i,N-1,1]**2 + v[i,N-1,2]**2)
+    E_t[i-1] = E_k[i-1] + U_g[i-1]
+
+# Create plot
+plt.figure()
+plt.plot(E_t)
+plt.title("Total Energy")
+plt.xlabel("Time")
+plt.ylabel("Energy")
+plt.savefig("Energy.png")
