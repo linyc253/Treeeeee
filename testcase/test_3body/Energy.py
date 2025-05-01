039f8abafc80bf4c653903e34a6339825446e761
teddyliu1025 <teddyliu02.1025@gmail.com>
2025-05-02 04:03:17 +0800

    Speed up the calculation of energy by fully vectorizing the caculation steps.

--- testcase/test_3body/Energy.py
+++ testcase/test_3body/Energy.py
@@ -41,14 +41,24 @@ for i in range(1, N_f):
 E_k = np.zeros(N_f-1)
 U_g = np.zeros(N_f-1)
 E_t = np.zeros(N_f-1)
-for i in range(1,N_f):
-    for j in range(N-1):
-        E_k[i-1] += 0.5 * m[j] * (v[i,j,0]**2 + v[i,j,1]**2 + v[i,j,2]**2)
-        for k in range(j+1,N):
-            U_g[i-1] -= G*m[j]*m[k]/((x[i,j,0]-x[i,k,0])**2 + (x[i,j,1]-x[i,k,1])**2 +\
-                                 (x[i,j,2]-x[i,k,2])**2)**0.5
-    E_k[i-1] += 0.5 * m[N-1] * (v[i,N-1,0]**2 + v[i,N-1,1]**2 + v[i,N-1,2]**2)
+for i in range(1, N_f):
+    # kinetic energy
+    E_k[i-1] = 0.5 * np.sum(m * np.sum(v[i]**2, axis=1))
+    
+    # potential energy
+    dx = x[i,:,np.newaxis,0] - x[i,np.newaxis,:,0]
+    dy = x[i,:,np.newaxis,1] - x[i,np.newaxis,:,1]
+    dz = x[i,:,np.newaxis,2] - x[i,np.newaxis,:,2]
+    r = np.sqrt(dx**2 + dy**2 + dz**2)
+    
+    m_outer = m[:,np.newaxis] * m[np.newaxis,:]
+    
+    upper = np.triu_indices(N, k=1)
+    U_g[i-1] = -G * np.sum(m_outer[upper] / r[upper])
+    
+    # total energy
     E_t[i-1] = E_k[i-1] + U_g[i-1]
+
 
 # Create plot
 plt.figure()
