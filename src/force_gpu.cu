b8f30acf3e7f182771b7b5d695de2897dd2ffcc5
linyc253 <linyc253@gmail.com>
2025-05-04 12:35:18 +0800

    I'm happy to announce the second version of rubbish (cuda kernel)

--- src/force_gpu.cu
+++ src/force_gpu.cu
@@ -4,63 +4,82 @@
 #include "tree.h"
 #include "particle.h"
 
-// Not optimized (too many global memory access)
-__global__ void Particle_Cell_Kernel(float4* P, int ng, float4* C, int nl, float3* F, float epsilon) {
+__device__ float3 two_partical_force_gpu(float4 a, float4 b, float3 f, float epsilon){
+    float3 dx;
+    float r_sq = 0.0;
+    dx.x = a.x - b.x;
+    dx.y = a.y - b.y;
+    dx.z = a.z - b.z;
+    r_sq = dx.x * dx.x + dx.y * dx.y + dx.z * dx.z;
+    float F_mag = -(a.w * b.w) / pow(r_sq + epsilon*epsilon, 1.5);
+    f.x += F_mag * dx.x;
+    f.y += F_mag * dx.y;
+    f.z += F_mag * dx.z;
+    return f;
+}
+
+__global__ void Particle_Cell_Kernel(float4* P, float4* C, int nl, float3* F, float epsilon) {
     int id = blockDim.x * blockIdx.x + threadIdx.x;
-    // Particle-Cell
-    if(id < ng * nl){
-        int ic = id / ng;
-        int ip = id % ng;
-        float3 dx;
-        float r_sq = 0.0;
-        dx.x = P[ip].x - C[ic].x;
-        dx.y = P[ip].y - C[ic].y;
-        dx.z = P[ip].z - C[ic].z;
-        r_sq = dx.x * dx.x + dx.y * dx.y + dx.z * dx.z;
-        float F_mag = -(P[ip].w * C[ic].w) / pow(r_sq + epsilon*epsilon, 1.5);
-        F[ip].x += F_mag * dx.x;
-        F[ip].y += F_mag * dx.y;
-        F[ip].z += F_mag * dx.z;
+    int ng = blockDim.x;
+
+    extern __shared__ float4 shPosition[];
+    float4 myPosition = P[threadIdx.x];
+
+    int i = id;
+    float3 f = {0.0, 0.0, 0.0};
+    while(i < nl + ng){
+        if(i < ng) shPosition[threadIdx.x] = P[i];
+        else shPosition[threadIdx.x] = C[i - ng];
+        __syncthreads();
+
+        for (int j = 0; j < blockDim.x; j++) {
+            //if(j == i && i < ng) continue;
+            f = two_partical_force_gpu(myPosition, shPosition[j], f, epsilon);
+        }
+        __syncthreads();
+
+        i += blockDim.x * gridDim.x;
     }
-    // Particle-Particle
-    else if(id < ng * (nl + ng)){
-        id -= ng * nl;
-        int ipp = id / ng;
-        int ip = id % ng;
-        float3 dx;
-        float r_sq = 0.0;
-        dx.x = P[ip].x - P[ipp].x;
-        dx.y = P[ip].y - P[ipp].y;
-        dx.z = P[ip].z - P[ipp].z;
-        r_sq = dx.x * dx.x + dx.y * dx.y + dx.z * dx.z;
-        float F_mag = -(P[ip].w * P[ipp].w) / pow(r_sq + epsilon*epsilon, 1.5);
-        F[ip].x += F_mag * dx.x;
-        F[ip].y += F_mag * dx.y;
-        F[ip].z += F_mag * dx.z;
+
+    F[id] = f;
+}
+
+__global__ void Force_Reduction_Kernel(float3* F){
+    int ib = blockDim.x;
+    int ng = gridDim.x;
+    while (ib != 0) {
+        if(threadIdx.x < ib){
+            F[threadIdx.x * ng + blockIdx.x].x += F[(threadIdx.x + ib) * ng + blockIdx.x].x;
+            F[threadIdx.x * ng + blockIdx.x].y += F[(threadIdx.x + ib) * ng + blockIdx.x].y;
+            F[threadIdx.x * ng + blockIdx.x].z += F[(threadIdx.x + ib) * ng + blockIdx.x].z;
+        }
+        __syncthreads();
+
+        ib /=2;
     }
-    __syncthreads();
 }
 
 extern "C" void Particle_Cell_Force_gpu(Coord4* P, int ng, Coord4* C, int nl, Coord3* F, double epsilon){
     float4 *d_P, *d_C;
     float3 *d_F;
 
+    int threadsPerBlock = ng;
+    int blocksPerGrid = 1;
+
     // Allocate device memory
     cudaMalloc((void**)&d_P, sizeof(float4) * ng);
     cudaMalloc((void**)&d_C, sizeof(float4) * nl);
-    cudaMalloc((void**)&d_F, sizeof(float3) * ng);
+    cudaMalloc((void**)&d_F, sizeof(float3) * ng * blocksPerGrid);
 
     // Transfer data from host to device memory
     cudaMemcpy(d_P, P, sizeof(float4) * ng, cudaMemcpyHostToDevice);
     cudaMemcpy(d_C, C, sizeof(float4) * nl, cudaMemcpyHostToDevice);
 
-    // Initialize d_F as 0.0 (https://forums.developer.nvidia.com/t/can-i-set-a-floats-to-zero-with-cudamemset/153706)
-    cudaMemset(d_F, 0, sizeof(float3) * ng);
-
     // Executing kernel
-    int threadsPerBlock = 1;
-    int blocksPerGrid = (ng * (nl + ng) + threadsPerBlock - 1) / threadsPerBlock; // ceil(ng * (nl + ng) / threadsPerBlock)
-    Particle_Cell_Kernel<<<blocksPerGrid, threadsPerBlock>>>(d_P, ng, d_C, nl, d_F, (float)epsilon);
+    int sm = threadsPerBlock*sizeof(float4);
+    Particle_Cell_Kernel<<<blocksPerGrid, threadsPerBlock, sm>>>(d_P, d_C, nl, d_F, (float)epsilon);
+    cudaDeviceSynchronize();
+    Force_Reduction_Kernel<<<threadsPerBlock, blocksPerGrid / 2>>>(d_F);
     cudaDeviceSynchronize();
 
     // Transfer data back to host memory
