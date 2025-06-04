a3a60fc30e8d4525c061e1ae2cc177cb517d3c4c
linyc253 <linyc253@gmail.com>
2025-06-04 11:43:27 +0800

    Modify parameters of test_double_plummer

--- testcase/test_double_plummer/run_all.sh
+++ testcase/test_double_plummer/run_all.sh
@@ -3,10 +3,9 @@ PYTHON=/home/linyc253/.conda/envs/env_1/bin/python
 
 ### Generate data (modfy N below)
 # N is the particle number for "each" plummer
-$PYTHON Initial_generator.py -N 5000 -V 1 > Initial.dat
-$PYTHON plot.py
+$PYTHON Initial_generator.py -N 500000 -V 1 > Initial.dat
 
-### Run METHOD = 2
+### Run
     cat > Input_Parameter.ini<<!
 [BasicSetting]
 DIM = 3                      # Dimension of the system (default: 3)
@@ -14,10 +13,10 @@ METHOD = 2                   # Method 1:brute_force
                              #        2:tree_algo (default)
 PARTICLE_FILE = Initial.dat  # filename of particle file (default: Initial.dat)
 T_TOT = 200                   # total evolution time
-DT = 0.3                    # maximal time interval
-ETA = 5.0                    # parameter that controls the accuracy and stability of the timestep in simulations
+DT = 0.1                    # maximal time interval
+ETA = 0.05                    # parameter that controls the accuracy and stability of the timestep in simulations
 TIME_PER_OUT = 0.5           # Output 00xxx.dat in every STEP_PER_OUT steps
-EPSILON = 5e-4               # softening length used to prevent singularities and numerical instabilities in particle interactions
+EPSILON = 0.004              # softening length used to prevent singularities and numerical instabilities in particle interactions
 OUTDIR = DATA
 
 [Tree]
@@ -26,15 +25,18 @@ POLES = 1
 NCRIT = 1000
 
 [Openmp]
-THREADS = 2
+THREADS = 4
 CHUNK = 1
+
+[GPU]
+threadsPerBlock = 128
 !
-../../bin/treeeeee > log
+../../bin/treeeeee
+
+$PYTHON plot_energy.py
 
 # Plot plummer animation
 cd Figure
 $PYTHON plot_gas.py -N 400
 cd ..
 ffmpeg -framerate 12 -i Figure/%05d.png plummer2.mp4
-# convert Figure/0*.png Plummer2.gif
-# $PYTHON Energy.py -N 200
